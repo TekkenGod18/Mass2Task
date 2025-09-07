0ddbc4683df44292893e0bb9e1308c9bd21f6bec
Administrator <admin@example.com>
2025-09-07 23:34:04 +0300

    Добавьте файлы проекта.

---
+++ Mass2Task/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mass2Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            int s = 0;
+            int k = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                if (array[i] % 2 == 0)
+                {
+                    s += array[i];
+                    k++;
+                }
+            }
+                Console.WriteLine(((double)s) / k);
+                Console.ReadKey();
+        }
+    }
+}
